be5bdba12216050ec839ec66cf9dc33ae93c3ee0
Mike Nadeau <mike@okto.ca>
2022-07-07 20:25:24 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
+/**
+ * Imports
+ */
+
 import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useParams
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	useParams
 } from "react-router-dom";
 
 import { Helmet, HelmetData } from 'react-helmet-async';
@@ -13,38 +17,67 @@ import './App.css';
 import { initializeApp } from "firebase/app"
 import { getFirestore, collection, addDoc } from "@firebase/firestore";
 
+/**
+ * Init Firebase
+ */
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB07inVKgYo4W2lh2UcJw88-i12iobf1vo",
-  authDomain: "okto-chat.firebaseapp.com",
-  databaseURL: "https://okto-chat-default-rtdb.firebaseio.com",
-  projectId: "okto-chat",
-  storageBucket: "okto-chat.appspot.com",
-  messagingSenderId: "314760933981",
-  appId: "1:314760933981:web:524278895599cc2e2ed3b4"
+	apiKey: "AIzaSyB07inVKgYo4W2lh2UcJw88-i12iobf1vo",
+	authDomain: "okto-chat.firebaseapp.com",
+	databaseURL: "https://okto-chat-default-rtdb.firebaseio.com",
+	projectId: "okto-chat",
+	storageBucket: "okto-chat.appspot.com",
+	messagingSenderId: "314760933981",
+	appId: "1:314760933981:web:524278895599cc2e2ed3b4"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+/**
+ * Init Helmet
+ */
+
 const helmetData = new HelmetData({});
 
+/**
+ * Video component
+ */
+
 const Video = () => {
-  const { lang } = useParams();
+	const { lang } = useParams();
 
-  var link = '';
-  if ( lang === 'fr' )
-    link = 'fr';
-  else if ( lang === 'en' )
-    link = 'en';
-  else
-    return ''; // Invalid or missing language
+	var link = '';
+	if ( lang === 'fr' )
+		link = 'https://vimeo.com/event/2251400/embed';
+	else if ( lang === 'en' )
+		link = 'https://vimeo.com/event/2265661/embed';
+	else
+		return ''; // Invalid or missing language
 
-  return (
-	<div>
-		Vimeo ({link})
-	</div>
-  );
+	return (
+		<div class="vimeo">
+			<div
+				style={{
+					padding: '56.25% 0 0 0',
+					position: 'relative',
+				}}
+			>
+				<iframe title="vimeo" src={link} frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style={{
+					position: 'absolute',
+					top: '0',
+					left: '0',
+					width: '100%',
+					height: '100%',
+				}}></iframe>
+			</div>
+		</div>
+	);
 };
+
+/**
+ * Chat component
+ */
 
 const Chat = () => {
   return (
@@ -57,6 +90,7 @@ const Chat = () => {
 function App() {
 
 	/*
+	Add to Firebase sample
 	const city = {
 		name: "Los Angeles",
 		state: "CA",
@@ -66,10 +100,13 @@ function App() {
 	*/
 
   return (
+
 	<Router>
+
 		<Helmet helmetData={helmetData}>
 
 		</Helmet>
+
 		<div class="wp-block-cover alignfull has-background-dim">
 			<img loading="lazy" width="2000" height="1125" class="wp-block-cover__image-background wp-image-5990" alt="" src="https://eoatownhall.ouronlineevent.com/wp-content/uploads/2022/02/Q1-2022-EL-Town-Hall-2.03.png" data-object-fit="cover" srcset="https://eltownhall.ouronlineevent.com/wp-content/uploads/2022/02/Q1-2022-EL-Town-Hall-2.03.png 2000w, https://eltownhall.ouronlineevent.com/wp-content/uploads/2022/02/Q1-2022-EL-Town-Hall-2.03-300x169.png 300w, https://eltownhall.ouronlineevent.com/wp-content/uploads/2022/02/Q1-2022-EL-Town-Hall-2.03-1024x576.png 1024w, https://eltownhall.ouronlineevent.com/wp-content/uploads/2022/02/Q1-2022-EL-Town-Hall-2.03-768x432.png 768w, https://eltownhall.ouronlineevent.com/wp-content/uploads/2022/02/Q1-2022-EL-Town-Hall-2.03-1536x864.png 1536w" sizes="(max-width: 2000px) 100vw, 2000px" />
 			<div class="wp-block-cover__inner-container">
@@ -80,18 +117,99 @@ function App() {
 				</div>
 			</div>
 		</div>
+
 		<div className="App">
 
-			<h1>Event is coming soon</h1>
-			<h2>July 21st - 3pm</h2>
+			<div class="agenda">
 
-			<br/><br/>
+				<h1>AGENDA</h1>
 
-			<h1>Événement disponible bientôt</h1>
-			<h2>21 Juillet - 15h</h2>
+				<div class="table">
+					<div class="row">
+						<div class="cell num">1</div>
+						<div class="cell">
+							<strong>Welcome<br/>
+							Bienvenue</strong><br/>
+							Rick Gadd, President EL Canada and Essilor Instruments NA
+						</div>
+					</div>
+					<div class="row">
+						<div class="cell num">2</div>
+						<div class="cell">
+							<strong>EssilorLuxottica Canada Story<br/>
+							L’histoire de EssilorLuxottica Canada</strong><br/>
+							Rick Gadd, President EL Canada and Essilor Instruments NA
+						</div>
+					</div>
+					<div class="row">
+						<div class="cell num">3</div>
+						<div class="cell">
+							<strong>Meet the Leadership Team<br/>
+							Rencontrer l’équipe de direction</strong><br/>
+							Canada Executive Leadership Team
+						</div>
+					</div>
+					<div class="row">
+						<div class="cell num">4</div>
+						<div class="cell">
+							<strong>New Ways of Working Update<br/>
+							Mise à jour des nouvelles méthodes de travail</strong><br/>
+							Isabelle Thériault, Vice President of Human Resources EL Canada
+						</div>
+					</div>
+					<div class="row">
+						<div class="cell num">5</div>
+						<div class="cell">
+							<strong>Eye to Eye with Leaders Open Q&amp;A<br/>
+							Partage de point de vue avec les leaders, questions et réponses ouvertes</strong><br/>
+							Canada Executive Leadership Team
+						</div>
+					</div>
+					<div class="row">
+						<div class="cell num">6</div>
+						<div class="cell">
+							<strong>Special Guest Speaker<br/>
+							Conférencier invité</strong><br/>
+							Maxime Laoun, Olympic Gold Medalist interviewed by Pascale Desroches, Sr. Marketing<br/>
+							Director EL Canada
+						</div>
+					</div>
+					<div class="row">
+						<div class="cell num">7</div>
+						<div class="cell">
+							<strong>Final Thoughts &amp; Close<br/>
+							Conclusion</strong><br/>
+							Rick Gadd
+						</div>
+					</div>
+					<div class="row">
+						<div class="cell num">8</div>
+						<div class="cell">
+							<strong>Happy Hour (4:30-5:30)
+							Cocktail (4:30-5:30)</strong>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+			<div class="buttons">
+				<a className="button" href="/fr">Français</a>
+				<a className="button" href="/en">English</a>
+			</div>
+
+			<Routes>
+				<Route exact path="/:lang" element={<Video/>}/>
+			</Routes>
+
+			<Routes>
+				<Route exact path="/:lang" element={<Chat/>}/>
+			</Routes>
 
 		</div>
+
 	</Router>
+
   );
 }
 
