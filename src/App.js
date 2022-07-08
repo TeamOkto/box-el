f24a1e0c8150d9032ee35c9a04d2c36038ef6a00
Mike Nadeau <mike@okto.ca>
2022-07-08 07:55:41 -0400

    update

--- src/App.js
+++ src/App.js
@@ -14,31 +14,24 @@ import { Helmet, HelmetData } from 'react-helmet-async';
 
 import './App.css';
 
-import { initializeApp } from "firebase/app"
-import { getFirestore, collection, addDoc } from "@firebase/firestore";
-
-/**
- * Init Firebase
- */
-
-const firebaseConfig = {
-	apiKey: "AIzaSyB07inVKgYo4W2lh2UcJw88-i12iobf1vo",
-	authDomain: "okto-chat.firebaseapp.com",
-	databaseURL: "https://okto-chat-default-rtdb.firebaseio.com",
-	projectId: "okto-chat",
-	storageBucket: "okto-chat.appspot.com",
-	messagingSenderId: "314760933981",
-	appId: "1:314760933981:web:524278895599cc2e2ed3b4"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+import Chat from './Chat.js'
 
 /**
  * Init Helmet
  */
 
 const helmetData = new HelmetData({});
+
+/**
+ * Chat loader
+ */
+
+const ChatLoader = () => {
+	const { lang } = useParams();
+	return (
+		<Chat lang={lang}/>
+	);
+};
 
 /**
  * Video component
@@ -75,31 +68,9 @@ const Video = () => {
 	);
 };
 
-/**
- * Chat component
- */
-
-const Chat = () => {
-  return (
-	<div className="App-chat">
-		Chat
-	</div>
-  );
-};
-
 function App() {
 
-	/*
-	Add to Firebase sample
-	const city = {
-		name: "Los Angeles",
-		state: "CA",
-		country: "USA"
-	}
-	addDoc(collection(db, 'cities'), city);
-	*/
-
-  return (
+	return (
 
 	<Router>
 
@@ -203,7 +174,7 @@ function App() {
 			</Routes>
 
 			<Routes>
-				<Route exact path="/:lang" element={<Chat/>}/>
+				<Route exact path="/:lang" element={<ChatLoader/>}/>
 			</Routes>
 
 		</div>
