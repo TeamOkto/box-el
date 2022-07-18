fe59c9131e73fa6ecb105ecd2b097fdb669d9bfe
Smart-UI web development agency <92440177+Company-Smart-ui@users.noreply.github.com>
2022-07-18 12:17:07 +0300

    new fix

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef } from 'react';
 import { useState } from 'react';
 import { db } from './firebase.config';
-import { 
+import 
+{ 
 	collection, 
 	addDoc, 
 	serverTimestamp, 
@@ -20,6 +21,11 @@ const Chat = () => {
 	const [stateChat, setStateChat] = useState(false);
 	const [getFetchingData, setGetFetchingData] = useState(true);
 
+	const ref = useRef();
+    const refSendMessage = useRef();
+	const refChatWrapper = useRef();
+	const scrollToBottom = useRef();
+
     const colRef = collection(db, "messages");
 
     useEffect(()=>{
@@ -28,65 +34,53 @@ const Chat = () => {
             querySnapshot.forEach(qs=>{
                 messagesUsers.push({...qs.data(), id: qs.id})
                 setMessages(messagesUsers);
-            })
-        })
-    },[getFetchingData])
+            });
+        });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[getFetchingData]);
 
-    const [data, setData] = useState({
-        name: '',
-        indef: 2
-    })
+	useEffect(() => {
+		refChatWrapper?.current?.scrollIntoView({behavior: "smooth"})
+	}, [messages]);
 
-    const ref = useRef();
-    const refSendMessage = useRef();
+	useEffect(() => {
+		refChatWrapper?.current?.scrollIntoView({behavior: "smooth"})
+	});
+
+	useEffect(() => {
+		scrollToBottom?.current?.addEventListener('scroll', scrollHandler);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		return () => scrollToBottom?.current?.removeEventListener('scroll', scrollHandler);
+	});
 
     const handlerClick = (event) => {
         event.preventDefault();
-        setData({name: ref.current.value})
         localStorage.setItem('user', ref.current.value);
         localStorage.setItem('indef', today.getMilliseconds());
     }
 
-    useEffect(()=> {
-        setData({name: localStorage.getItem('user'), indef: localStorage.getItem('indef')})
-    }, [])
-
-	const refChatWrapper = useRef();
-	const scrollToBottom = useRef();
-
-
-	useEffect(() => {
-		refChatWrapper?.current?.scrollIntoView({behavior: "smooth"})
-	}, [messages])
-
     const sendMessageHandler = (e) => {
-        e.preventDefault();
-		if(refSendMessage?.current?.value !== ''){
+		e.preventDefault()
+		
+		if(refSendMessage?.current?.value.trim() === ''){
+			return refSendMessage.current.value = '';
+			
+		}else{
 			addDoc(colRef, {
 				indef: localStorage.getItem('indef'),
 				fullname: localStorage.getItem('user'),
-				message: refSendMessage?.current?.value,
+				message: refSendMessage?.current?.value.trim() !== '' && refSendMessage?.current?.value,
 				timestamp: serverTimestamp()
 			});
 			refSendMessage.current.value = '';
 		}
-    }
+    };
 
-	useEffect(() => {
-		refChatWrapper?.current?.scrollIntoView({behavior: "smooth"})
-	})
-
-	useEffect(() => {
-		scrollToBottom?.current?.addEventListener('scroll', scrollHandler);
-		return () => scrollToBottom?.current?.removeEventListener('scroll', scrollHandler);
-	});
-
-
-	const scrollHandler = (e) => {
+	const scrollHandler = () => {
 		if(scrollToBottom.current.scrollHeight - (scrollToBottom.current.scrollTop + window.innerHeight) < 10){
 			setGetFetchingData(true);
-		}
-	}
+		};
+	};
 
 	return(
 		<div className={"chat_wrapper"}>
@@ -104,9 +98,12 @@ const Chat = () => {
 			<>
 				<div ref={scrollToBottom} className="chat_content__wrapper">
 					{messages.map((message, index) => (
-						<div key={index} className={message.indef == localStorage.getItem('indef') ? 'chat_content__wrapper_box owner-message' : 'chat_content__wrapper_box'}>
+						<div 
+							key={index} 
+							className={message.indef === localStorage.getItem('indef') ? 'chat_content__wrapper_box owner-message' : 'chat_content__wrapper_box'}
+						>
 							<div 
-								style={message.indef == localStorage.getItem('indef') ? {color: '#326fa8'} : {}}
+								style={message.indef === localStorage.getItem('indef') ? {color: '#326fa8'} : {}}
 								className="chat_content__wrapper_userName">{message?.fullname}
 							</div>
 							<div>{message?.message}</div>
@@ -115,10 +112,18 @@ const Chat = () => {
 					<div ref={refChatWrapper}/>
 				</div>
 				<form className="chat_wrapper__footer">
-					<input ref={refSendMessage} type="text" className="chat_wrapper__footer_input input_styles" placeholder='type here' required/>
+					<input 
+						ref={refSendMessage} 
+						type="text" 
+						className="chat_wrapper__footer_input input_styles" 
+						placeholder='type here' 
+						min={1} 
+						minLength={1}
+						required
+					/>	
 					<button type='submit' className="chat_wrapper__footer_send" onClick={sendMessageHandler}>
-						<img src='/send_message.png' alt='send'/>
-					</button>	
+							<img src='/send_message.png' alt='send'/>
+					</button>
 				</form>
 				</>
 				}
@@ -128,7 +133,15 @@ const Chat = () => {
 			<div className="chat_wrapper__auth">
 				<h2>Please enter your full name</h2>
 				<form>
-					<input ref={ref} type="text" minLength={3} maxLength={15} className="input_styles" placeholder='type here' required/>
+					<input 
+						ref={ref} 
+						type="text" 
+						minLength={3} 
+						maxLength={15} 
+						className="input_styles" 
+						placeholder='type here' 
+						required
+					/>
 					<button type='submit' onClick={handlerClick}>Confirm</button>
 				</form>
 			</div>
