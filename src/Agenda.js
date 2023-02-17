cd697ff701f417d9a829fd2bfee6dfd3a0a6fc55
root <root@bajor.okto.ca>
2023-02-17 13:12:11 -0500

    update 2023

--- src/Agenda.js
+++ src/Agenda.js
@@ -24,86 +24,52 @@ export default class Agenda extends Component {
 								<div className="cell num">1</div>
 								<div className="cell">
 									<strong>
-										Welcome, Q4 recap<br/>
-										Bienvenue, Récapitulatif du 4ème trimestre
+										Welcome / Bienvenue<br/>
+										2023 Business Objectives<br/>
+										Objectifs d’affaires 2023
 									</strong><br/>
-									Rick Gadd, President of Wholesale Canada for Essilor Luxottica &amp; Instruments, North America<br/>
-									10 minutes
+									Rick Gadd, President of Wholesale Canada for Essilor Luxottica &amp; Instruments, North America
 								</div>
 							</div>
 							<div className="row">
 								<div className="cell num">2</div>
 								<div className="cell">
 									<strong>
-										2023 Business Objectives<br/>
-										Objectifs d’affaires 2023
+										Marketing Strategy for 2023<br/>
+										Stratégie Marketing pour 2023
 									</strong><br/>
-									Rick Gadd, President of Wholesale Canada for Essilor Luxottica &amp; Instruments, North America<br/>
-									10 minutes
+									Pascale Desroches, Head of Marketing, Essilor Luxottica Canada
 								</div>
 							</div>
 							<div className="row">
 								<div className="cell num">3</div>
 								<div className="cell">
 									<strong>
-										Marketing Strategy for 2023<br/>
-										Stratégie Marketing pour 2023
+										Frames Overview<br/>
+										Survol des montures
 									</strong><br/>
-									Pascale Desroches, Head of Marketing, Essilor Luxottica Canada<br/>
-									10 minutes
+									Ken Clark, National Director of Sales, Essilor Luxottica Canada<br/>
+									Marty Hosseiny, Sales Manager, Key Accounts, Essilor Luxottica Canada
 								</div>
 							</div>
 							<div className="row">
 								<div className="cell num">4</div>
 								<div className="cell">
 									<strong>
-										Frames Overview<br/>
-										Survol des montures
+										Human Ressources Update<br/>
+										Mise à jour des Ressources humaines
 									</strong><br/>
-									Ken Clark, National Director of Sales, Essilor Luxottica Canada<br/>
-									Marty Hosseiny, Sales Manager, Key Accounts, Essilor Luxottica Canada<br/>
-									10 minutes
+									Isabelle Thériault, Vice présidente, Ressources humaines, Essilor Luxottica Canada
 								</div>
 							</div>
 							<div className="row">
 								<div className="cell num">5</div>
 								<div className="cell">
 									<strong>
-										Human Ressources Update<br/>
-										Mise à jour des Ressources humaines
-									</strong><br/>
-									Isabelle Thériault, Vice présidente, Ressources humaines, Essilor Luxottica Canada<br/>
-									10 minutes
-								</div>
-							</div>
-							<div className="row">
-								<div className="cell num">6</div>
-								<div className="cell">
-									<strong>
 										Update on OneSight Essilor Luxottica Foundation Canada <br/>
 										Mise à jour sur la Fondation OneSight Essilor Luxottica Foundation Canada
 									</strong><br/>
-									Kris Kelm, Community Impact Manager, OneSight Essilor Luxottica Foundation, Canada<br/>
-									10 minutes
-								</div>
-							</div>
-							<div className="row">
-								<div className="cell num">7</div>
-								<div className="cell">
-									<strong>
-										Q&A<br/>
-										Questions et réponses
-									</strong><br/>
-									10-15 minutes
-								</div>
-							</div>
-							<div className="row">
-								<div className="cell num">8</div>
-								<div className="cell">
-									<strong>
-										Coffee and sweets (3:00-4:00)<br/>
-										Café et gourmandises (3:00-4:00)
-									</strong>
+									Kris Kelm, Community Impact Manager, OneSight Essilor Luxottica Foundation, Canada
 								</div>
 							</div>
 						</div>
